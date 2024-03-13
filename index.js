50b62252251537be5a94d404ce80fc5ffe145531
fazlerabbi13 <mohammad0132794@gmail.com>
2024-03-14 00:56:17 +0600

    checking existing user by query

--- index.js
+++ index.js
@@ -41,6 +41,11 @@ async function run() {
 
         app.post('/users', async(req, res) =>{
             const user = req.body;
+            const query ={email:user.email};
+            const existingUser = await usersCollection.findOne(query);
+            if(existingUser){
+                return res.send({message:'user already exist', insertedId:null})
+            }
             const result = await usersCollection.insertOne(user);
             res.send(result); 
         })
